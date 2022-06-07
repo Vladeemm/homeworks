fb765975d70833cf1feaa8212b11e255ebce7330
Владимир Гаврилов <Vladeem@rambler.ru>
2022-06-08 01:42:43 +0300

    first-task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
+  "use strict";
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+    arr = [];
+    let x;
+    let D = b**2 - 4 * a * c;
+      if (D < 0) {
+        arr = [];
+      } else if (D === 0) {
+        x = -b / (2 * a);
+        arr.push(x);
+      } else if (D > 0) {
+        x = (-b + Math.sqrt(D) ) / (2 * a) 
+          arr.push(x);
+        x = (-b - Math.sqrt(D) ) / (2 * a)
+          arr.push(x);
+      }
   return arr; // array
 }
 
