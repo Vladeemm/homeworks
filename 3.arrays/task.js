ffbf7d898cf1a552a59bbfa44f411a80454664c7
Владимир Гаврилов <Vladeem@rambler.ru>
2022-08-13 21:47:14 +0500

    Extended work with arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((arr1, i) => arr1 === arr2[i]);
 
   return result; // boolean
 }
@@ -9,7 +9,12 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  const positiveNumbers = arr.filter((idx) => idx > 0);
+    resultArr = positiveNumbers;
+  const multipleOfThree = resultArr.filter((idx) => idx % 3 === 0);
+    resultArr = multipleOfThree;
+  const result = resultArr.map((idx) => idx * 10);
+    resultArr = result;
 
   return resultArr; // array
 }
