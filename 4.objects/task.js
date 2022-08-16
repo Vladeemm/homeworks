bc3e9fe57b887c78e405836e525f1ba8ac4a0731
Владимир Гаврилов <Vladeem@rambler.ru>
2022-08-16 13:59:09 +0500

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,33 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  return this.subject;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+    this.marks.push(mark);
+    } else {
+    this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (mark1, mark2, ...mark3) {
+  return this.marks.push(mark);
+}
+
+Student.prototype.getAverage = function () {
+  return mark / mark.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete subject;
+  delete marks;
+  const excluded = { excluded: 'excluded!' };
+}
