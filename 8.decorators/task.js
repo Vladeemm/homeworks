eb50b96f5ec2693376590b4dcd876c275df5544d
Владимир Гаврилов <Vladeem@rambler.ru>
2022-09-17 06:18:26 +0300

    5, 6, 7, 8 homeworks

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,43 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === hash);
+    if (idx !== -1) { 
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } else {
+      let result = func(...args); 
+      cache.push({ hash: hash, value: result }); 
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(f, ms) {
+  let isCooldown = false;
+  return function () {
+    if (isCooldown) return;
+    f.apply(this, arguments);
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, ms);
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let isCooldown = false,
+   count;
+  return function () {
+    if (isCooldown) return;
+    func.apply(this, arguments);
+    isCooldown = true;
+    setTimeout(() => isCooldown = false, ms);
+    count++;
+  };
 }
